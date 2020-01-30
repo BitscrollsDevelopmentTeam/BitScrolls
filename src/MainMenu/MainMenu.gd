669a90dcea5ae44fd236e00c122fabf32e515db2
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-01-30 20:16:33 +0400

    Added functionality to Menu Buttons

---
+++ src/MainMenu/MainMenu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+func _ready():
+	for button in $Menu/CenterRow/Buttons.get_children():
+		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+		
+
+func _on_Button_pressed(scene_to_load):
+	get_tree().change_scene(scene_to_load)
