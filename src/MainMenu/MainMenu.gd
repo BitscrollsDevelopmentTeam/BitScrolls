9df9893d23b74320dca075a3a793f86ad5a43322
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-02-01 13:48:31 +0400

    Added some cool animations

--- src/MainMenu/MainMenu.gd
+++ src/MainMenu/MainMenu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+onready var fade = $Fade
 
 func _ready():
 	for button in $Menu/CenterRow/Buttons.get_children():
@@ -8,6 +9,11 @@ func _ready():
 
 func _on_Button_pressed(scene_to_load):
 	get_tree().change_scene(scene_to_load)
+	
 
 func _on_ExitButton_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	fade.hide()
