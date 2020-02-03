d63f0d66d8c70e468f51d9dbe4d0af342c73ada2
MysteryCoder456 <rehatbir.singh@gmail.com>
2020-02-03 15:50:57 +0400

    Finished new menu Design

---
+++ src/MainMenu/MainMenu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_StartButton_pressed() -> void:
+	get_tree().change_scene("res://src/Levels/World.tscn")
+
+
+func _on_OptionsButton_pressed() -> void:
+	get_tree().change_scene("res://src/OptionsMenu/OptionsMenu.tscn")
+
+
+func _on_ExitButton_pressed() -> void:
+	get_tree().quit()
